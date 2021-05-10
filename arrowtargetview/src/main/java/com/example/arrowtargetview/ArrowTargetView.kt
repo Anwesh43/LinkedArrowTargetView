98ddd875208938c8c3db409f58063af66b2ec9ce
Anwesh43 <anweshthecool0@gmail.com>
2021-05-11 02:10:33 +0530

    addin ArrowTarget class

--- arrowtargetview/src/main/java/com/example/arrowtargetview/ArrowTargetView.kt
+++ arrowtargetview/src/main/java/com/example/arrowtargetview/ArrowTargetView.kt
@@ -185,4 +185,27 @@ class ArrowTargetView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArrowTarget(var i : Int) {
+
+        private var curr : ATNode = ATNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
